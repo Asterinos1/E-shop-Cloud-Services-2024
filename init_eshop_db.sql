18af76394e132a2db63c5e515b4e0cb7fcb35d6d
Asterinos Karalis <karalhs.as@gmail.com>
2024-11-07 18:38:41 +0200

    Increased url length for images

--- init_eshop_db.sql
+++ init_eshop_db.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS products (
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     price NUMERIC(10, 2) NOT NULL,
-    image_url VARCHAR(255),
+    image_url VARCHAR(1024),
     quantity INTEGER NOT NULL
 );
